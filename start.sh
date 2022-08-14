c25ceaad7f4131b01392341455acd5f82f036da1
shiguoliang <shiguoliang19@outlook.com>
2022-08-14 12:52:15 +0800

    docker test

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 docker build -t slideshow-go .
 
-docker run -d -p 8085:8085 -v /opt/assets/slideshow-go/src/assets/images:/data/assets/images --restart=always slideshow-go
+docker run -d -p 8085:8085 -v /opt/assets/slideshow-go/src/assets/images:/data/assets/images -v /opt/assets/slideshow-go/src/slideshow.db:/data/slideshow.db --restart=always slideshow-go
